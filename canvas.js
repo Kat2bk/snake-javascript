a946898cc2238367bc0475003031c91eec1c8029
unknown <Kat2bk@gmail.com>
2020-03-26 18:30:40 -0700

    snake object is displaying on canvas... must make it bigger however... going to figure that out now

--- canvas.js
+++ canvas.js
@@ -20,4 +20,7 @@ var snake;
 // setter function
 //  In JavaScript, a setter can be used to execute a function whenever a specified property is attempted to be changed. Setters are most often used in conjunction with getters to create a type of pseudo-property. It is not possible to simultaneously have a setter on a property that holds an actual value.
 
-funct;
+(function setup() {
+  snake = new Snake();
+  snake.draw();
+})();

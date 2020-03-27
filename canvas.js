9c19b3cfc05efada04954eb07abc5be9d4829656
unknown <Kat2bk@gmail.com>
2020-03-26 18:27:56 -0700

    deleted some files to simplify... this way is easier to learn. have three files for now. one for canvas... one for snake object and the other for html

---
+++ canvas.js
@@ -0,0 +1,23 @@
+// core library - game files
+
+// The getContext() method returns an object that provides methods and properties for drawing on the canvas. This reference will cover the properties and methods of the getContext("2d") object, which can be used to draw text, lines, boxes, circles, and more - on the canvas.
+
+// var ctx = canvas.getContext(contextType, contextAttributes);
+// "2d", leading to the creation of a CanvasRenderingContext2D object representing a two-dimensional rendering context.
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// we want to get this canvas into rows and columns to have a function for initial state and for the snake to move
+
+const pixel = 10;
+
+const rows = canvas.height / pixel;
+const columns = canvas.width / pixel;
+
+var snake;
+
+// setter function
+//  In JavaScript, a setter can be used to execute a function whenever a specified property is attempted to be changed. Setters are most often used in conjunction with getters to create a type of pseudo-property. It is not possible to simultaneously have a setter on a property that holds an actual value.
+
+funct;
--- snake.js
+++ snake.js
@@ -1,1 +1,15 @@
-// core library - game files
+// constructor function...
+// https://www.w3schools.com/js/js_object_constructors.asp
+
+// must set an initial value for snake
+// https://www.w3schools.com/tags/canvas_fillrect.asp
+
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+
+  this.draw = function() {
+    ctx.fillStyle = "#FFFFF";
+    ctx.fillRect(this.x, this.y, pixel, pixel);
+  };
+}
